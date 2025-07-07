e2b5aaaf4307ac4294df0edbfe9dfe5e76454d85
Ahmed <akhanji01@gmail.com>
2025-07-06 22:35:59 -0400

    first stage backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('balance/', views.get_balance),
+    path('assets', views.get_assets),
+    path('fiat', views.get_usd_left),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render
-from django.http import JsonResponse, Response
+from django.http import JsonResponse
 import ccxt
 
-def get_info_ccxt(request):
-    return Response()
+QUOTE_CURRENCY = 'USD'
+api_key = 'yqPWrtVuElaIExKmIp/E/upTOz/to1x7tC3JoFUxoSTKWCOorT6ifF/B'
+api_secret = 'L8h5vYoAu/jpQiBROA9yKN41FGwZAGGVF3nfrC5f5EiaoF7VksruPVdD7x1VOwnyyNCMdrGnT8lP4xHTiBrYMQ=='
+exchange = ccxt.kraken({
+    'apiKey': api_key,
+    'secret': api_secret,
+    'options': {
+        'defaultType': 'spot',  # Ensure only spot markets are considered (no futures)
+    }
+})
+
+# return current overall money (owned assets + fiat) at this moment in the porfolio (excluding locked assets)
+def get_balance(request):
+    try:
+        balances = exchange.fetch_balance() # Fetch current account balances (both free and used)
+        tickers = exchange.fetch_tickers() # Fetch the latest market prices for all tickers
+        portfolio_value = 0.0
+
+        # Loop through all assets in the portfolio
+        for currency, balance in balances['total'].items():
+            if balance > 0:
+                if currency == QUOTE_CURRENCY:
+                    portfolio_value += balance
+                else:
+                    # Try to find a trading pair to convert this asset to USD
+                    pair = f"{currency}/{QUOTE_CURRENCY}"
+                    if pair in exchange.markets:
+                        price = tickers[pair]['last']
+                        portfolio_value += balance * price
+        
+        return JsonResponse({'balance': round(portfolio_value, 2)})
+    except ccxt.BaseError as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+
+# return current fiat (available usd in portfolio)
+def get_usd_left(request):
+    available = exchange.fetch_balance()['free'].get(QUOTE_CURRENCY, 0)
+    return JsonResponse({'available': round(available, 2)})
+
+# return assets owned ([{symbol: amount}, ...])
+def get_assets(request):
+    balances = exchange.fetch_balance()
+    assets = [
+        {'symbol': symbol, 'amount': amount}
+        for symbol, amount in balances['total'].items()
+        if amount > 0
+    ]
+    return JsonResponse(assets, safe=False)
