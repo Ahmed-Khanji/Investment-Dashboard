b51c5bf76d9ddb3670a9333de6b8fa2bfb2f582b
Ahmed <akhanji01@gmail.com>
2025-07-09 18:16:30 -0400

    setting up control panel

--- frontend/src/components/ControlPanel.jsx
+++ frontend/src/components/ControlPanel.jsx
@@ -2,7 +2,33 @@ import { useState, useEffect } from 'react';
 import '../styles/ControlPanel.css';
 
 export default function ControlPanel() {
-    
+    // states for the metrics
+    const [mthlyReturns, setMthlyReturns] = useState([]);
+    const [sharpRatio, setSharpRatio] = useState(0);
+    const [sortinoRatio, setSortinoRatio] = useState(0);
+    const [maxDrawDown, setMaxDrawDown] = useState(0);
+    const [avgDrawDown, setAvgDrawDown] = useState(0);
+    const [avgDownDuration, setAvgDownDuration] = useState(0);
+    const [mthWinRate, setMthWinRate] = useState(0);
+    // 2 other after
+
+    // fetch statArray 
+    const [statArray, setStatArray] = useState([]);
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/all-balances/')
+            .then(res => res.json())
+            .then(data => {
+                setStatArray(data);
+            })
+            .catch(err => console.error(err.message));
+    }, []);
+
+    // calculate monthly Return
+    function computeMontlyReturn() {
+        mthlyReturns = statArray.map((stat, i) => {
+            
+        });
+    }
 
     return (
         <> 
